4a9fa75b78711b094ba991f4f380f9a3c37f3805
karlpet <karlpetter.svensson@hotmail.se>
2016-11-25 22:10:33 +0100

    weed

--- xml-converter/raw-data-converter.js
+++ xml-converter/raw-data-converter.js
@@ -1,15 +1,26 @@
 var fs = require("fs");
 var parseString = require('xml2js').parseString;
 
-var dailyusageobj = {
-  date: "",
-  hourlyusage: []
-};
 
 var xml = fs.readFileSync(__dirname + "/xml-originals/TestGBDataThirteenMonthsBinnedDaily.xml");
 parseString(xml, function (err, result) {
+
+  result.feed.entry.splice(0,4);
+  result.feed.entry.splice(result.feed.entry.length - 1, 1)
+
   for (var day of result.feed.entry) {
-    console.log(day);
+    var dailyusageobj = {
+      date: "",
+      hourlyusage: []
+    };
+    dailyusageobj.date = day.published[0].slice(0,10);
+    var hourlyusagearray = [];
+
+    for (var hourlyusage of day.content[0].IntervalBlock[0].IntervalReading) {
+      hourlyusagearray.push(hourlyusage.value[0]);
+    }
+    dailyusageobj.hourlyusage.push(hourlyusagearray);
+    console.log(dailyusageobj);
   }
   //fs.writeFileSync(__dirname + "/data.json", JSON.stringify(result, null, 4));
 });
