d5ef82df5af9fff09411456acd6a2cb31d8c17f8
Oskar Råhlén <rahlenoskar@gmail.com>
2016-11-25 22:13:23 +0100

    ading future json stuff

---
+++ Future/GettingFutureData.js
@@ -0,0 +1,26 @@
+var nordpool = require('nordpool');
+var prices = new nordpool.Prices();
+
+var opts = {}
+//opts.currency = 'SEK'
+//opts.area = 'SE1' // DK2, EE1, FI, 'Kr.sand', LT, LV, 'Oslo', SE1, SE4, ...
+//opts.from = '2016-11-26'
+
+// curr = SEK area = S1 and callback is a function that gets the data. (results)
+module.exports = function(curr, area, callback){
+  var currentDate = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
+  var day = currentDate.getDate()
+  var month = currentDate.getMonth() + 1
+  var year = currentDate.getFullYear()
+  var date = year + "-" + month + "-" + day;
+
+  opts.currency = curr;
+  opts.area = area;
+  opts.from = date;
+
+  prices.hourly(opts, function (error, results) {
+    if (error) console.err(error);
+
+    callback(results);
+  })
+}
---
+++ Future/HandelingFutureData.js
@@ -0,0 +1,11 @@
+var futureData = require('./GettingFuturedata');
+
+
+//var cool
+
+
+//module.exports.getHighestCost = function(callback){
+  //var data =futureData('SEK', 'S1', cool);
+//}
+
+// highest, lowest, input-output
