df7f03fb88e8fa240f90c03af27db57ceceb9bbe
Oskar Råhlén <rahlenoskar@gmail.com>
2016-11-25 22:58:25 +0100

    added functions to the future data

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var path = require("path");
+var handelingFutureData = require('./Future/HandelingFutureData');
+var futureData = require('./Future/GettingFuturedata');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -14,6 +16,21 @@ app.get('/', function(request, response) {
   response.render('index');
 });
 
+var callingGetFutureData = function(results){
+  var highestCost = handelingFutureData.getHighestCost(results);
+  var timeForHighestCost = handelingFutureData.getTimeByCost(highestCost, results);
+  var lowestCost = handelingFutureData.getLowestCost(results);
+  var fet = handelingFutureData.getCostByTime(10, results);
+  console.log(fet);
+  //console.log(lowestCost);
+  //console.log(timeForHighestCost);
+  //console.log(highestCost);
+}
+
+
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+  var data = new futureData('SEK', 'SE1', callingGetFutureData);
+
 });
