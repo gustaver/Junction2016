72291dcfd334c131ea3d3e7369434137e58c97c1
karlpet <karlpetter.svensson@hotmail.se>
2016-11-25 22:49:13 +0100

    metoden getDailyUsageData() implementerad

--- xml-converter/raw-data-converter.js
+++ xml-converter/raw-data-converter.js
@@ -2,33 +2,37 @@ var fs = require("fs");
 var parseString = require('xml2js').parseString;
 
 
-var xml = fs.readFileSync(__dirname + "/xml-originals/TestGBDataThirteenMonthsBinnedDaily.xml");
-parseString(xml, function (err, result) {
+var getDailyUsageData = function() {
+  return new Promise((resolve, reject) => {
+    var xml = fs.readFileSync(__dirname + "/xml-originals/TestGBDataThirteenMonthsBinnedDaily.xml");
 
-  result.feed.entry.splice(0,4);
-  result.feed.entry.splice(result.feed.entry.length - 1, 1)
-
-  var count = 0;
-
-  for (var day of result.feed.entry) {
-    var dailyusageobj = {
-      date: "",
-      hourlyusage: []
-    };
-    dailyusageobj.date = day.published[0].slice(0,10);
-    var hourlyusagearray = [];
-
-    for (var hourlyusage of day.content[0].IntervalBlock[0].IntervalReading) {
-      hourlyusagearray.push(hourlyusage.value[0]);
+    var dailyUsageData = {
+      data: []
     }
-    dailyusageobj.hourlyusage.push(hourlyusagearray);
-    var filename = "day"
-    if (count < 10) { filename += "0" }
-    if (count < 100) { filename += "0" }
-    filename += count;
-    filename += ".json";
-    fs.writeFileSync(__dirname + "/daily/" + filename, JSON.stringify(dailyusageobj, null, 4))
-    console.log("dailyusageobj with date " + dailyusageobj.date + " written to daily/" + filename + " folder");
-    count += 1;
-  }
-});
+
+    parseString(xml, (err, result) => {
+
+      result.feed.entry.splice(0,4);
+      result.feed.entry.splice(result.feed.entry.length - 1, 1)
+
+      for (var day of result.feed.entry) {
+        var dailyusageobj = {
+          date: "",
+          hourlyusage: []
+        };
+        dailyusageobj.date = day.published[0].slice(0,10);
+        var hourlyusagearray = [];
+
+        for (var hourlyusage of day.content[0].IntervalBlock[0].IntervalReading) {
+          hourlyusagearray.push(hourlyusage.value[0]);
+        }
+        dailyusageobj.hourlyusage.push(hourlyusagearray);
+        dailyUsageData.data.push(dailyusageobj);
+      }
+
+      resolve(dailyUsageData);
+    });
+  });
+}
+
+module.exports.getDailyUsageData = getDailyUsageData;
