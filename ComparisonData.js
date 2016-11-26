e095be2daba5317d87f3b9a428f8170d6edbca96
karlpet <karlpetter.svensson@hotmail.se>
2016-11-26 10:00:58 +0100

    data zipping is tenderly taken care of

--- ComparisonData.js
+++ ComparisonData.js
@@ -1,6 +1,6 @@
 var handelingFutureData = require(__dirname  + '/Future/HandelingFutureData');
-var futureData = require(__dirname + '/Future/GettingFuturedata');
-
+var futureData = require(__dirname + '/Future/GettingFutureData');
+var dailyUsageData = require(__dirname + "/xml-converter/raw-data-converter")
 
 var callingGetFutureData = function(results){
 
@@ -9,14 +9,26 @@ var callingGetFutureData = function(results){
   var timeForHighestCost = handelingFutureData.getTimeByCost(highestCost, results);
   var lowestCost = handelingFutureData.getLowestCost(results);
   var fet = handelingFutureData.getCostByTime(10, results);
-  console.log(fet);
-  console.log(lowestCost);
-  console.log(timeForHighestCost);
-  console.log(highestCost);
 }
 
-
 module.exports = function(){
-  var data = new futureData('SEK', 'SE1', callingGetFutureData);
+  var mainData = {
+    data: []
+  }
+  var futureDataArray = [];
 
+  var data = new futureData('SEK', 'SE1').then(result => {
+
+    futureDataArray = result;
+    return dailyUsageData.getDailyUsageData()
+  }).then(usageData => {
+    var usageDataArray = usageData.data[usageData.data.length - 1].hourlyusage[0];
+    for (var i = 0; i < futureDataArray.length; i++) {
+      futureDataArray[i].usage = ( usageDataArray[i] / 1000 );
+      futureDataArray[i].cost = ( futureDataArray[i].value * futureDataArray[i].usage );
+      futureDataArray[i].hour = futureDataArray[i].date.format("HH:mm:ss");
+    }
+
+    console.log(futureDataArray);
+  });
 }
