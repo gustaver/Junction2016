0e084dab748edf899adf563738451e2131e57d1e
Gustave Rousselet <gustave.rousselet@icloud.com>
2016-11-27 00:44:26 +0200

    Add text message funcationality to smart notifications.

--- ComparisonData.js
+++ ComparisonData.js
@@ -177,6 +177,8 @@ module.exports.getNotifications = function(lowerEnd, upperEnd) {
                 var type = "suggestion";
                 notificationArray[j].message = suggestionMessage;
                 notificationArray[j].type = type;
+                var textMessage = "Low price hour!" + "\n" + "Move your power usage to save: " + highest1.hour + ":00" + "->" + current.hour + "=" + "+" + difference1 + " kr." + "\n" + highest2.hour + ":00" + "->" + current.hour + "=" + "+" + difference2 + " kr." + "\n" + highest3.hour + ":00" + "->" + current.hour + "=" + "+" + difference3 + " kr.";
+                notificationArray[j].textmessage = textMessage;
             }
         }
     }
