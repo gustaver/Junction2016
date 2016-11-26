4e5f707e87f738e9e9c6b7bdaee7aaa30c62fa4f
Oskar Råhlén <rahlenoskar@gmail.com>
2016-11-27 00:30:51 +0100

    some backend stuff

--- Routes.js
+++ Routes.js
@@ -4,8 +4,9 @@ var notifications = require(__dirname + "/ComparisonData");
 
 
 router.get("/today", function(req, res, next){
-  var start = parseInt(req.param('start')) || 6
-  var end = parseInt(req.param('end')) || 22
+  var start = parseInt(req.query.from) || 6
+  var end = parseInt(req.query.to) || 22
+  console.log(start);
   if(start => 0 && start < 23 && end > 0 && end < 24 && end - start > 0){
     notifications.getNotifications(parseInt(start), parseInt(end)).then(result => {
     res.send(result);
