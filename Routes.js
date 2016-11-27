abb5be0c13bcf5a1025d71fa6e3d06c341e87188
Oskar Råhlén <rahlenoskar@gmail.com>
2016-11-27 09:44:09 +0100

    sacharias 600 nobb

--- Routes.js
+++ Routes.js
@@ -7,15 +7,21 @@ router.get("/today", function(req, res, next){
   var start = parseInt(req.query.from) || 6
   var end = parseInt(req.query.to) || 22
   console.log(start);
-  if(start => 0 && start < 23 && end > 0 && end < 24 && end - start > 0){
-    notifications.getNotifications(parseInt(start), parseInt(end)).then(result => {
-    res.send(result);
-  });
-} else{
-  notifications.getNotifications(6, 22).then(result => {
-  res.send(result);
-});
+  if(start >=	0 && start < 23 && end > 0 && end < 24 && end - start > 0){
+
+} else {
+  start = 6;
+  end = 22;
 }
+
+notifications.getNotifications(parseInt(start), parseInt(end)).then(result => {
+res.send(result);
+});
+//else{
+  //notifications.getNotifications(6, 22).then(result => {
+  //res.send(result);
+//});
+//}
 
 });
 
